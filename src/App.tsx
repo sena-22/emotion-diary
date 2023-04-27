c421fb0c90520c9c2dedae91ac95128966f4187b
sena-22 <senika_@naver.com>
2023-04-27 19:13:26 +0900

    :sparkles: refactor: login 기능, 페이지 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,41 @@
-import React from "react"
+import React, {useEffect} from "react"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 import "./App.css"
-import {Diary, Edit, Home, New} from "./pages/index"
+import {auth} from "./firebase"
+import {Diary, Edit, Home, New, Login} from "./pages/index"
+import useLoginStore from "./zustand/userStore"
 
 function App() {
+  const {user, login, logout} = useLoginStore()
+
+  useEffect(() => {
+    auth.onAuthStateChanged((loginUser) => {
+      if (loginUser) {
+        login({
+          uid: loginUser.uid,
+          photo: loginUser.photoURL,
+          email: loginUser.email,
+          displayName: loginUser.displayName,
+        })
+      } else {
+        logout()
+      }
+    })
+  }, [])
+
   return (
     <>
       <BrowserRouter>
         <div className="min-h-screen px-5 sm:w-[90vw] md:w-[640px]">
           <Routes>
-            <Route path="/" element={<Home />}></Route>
-            <Route path="/new" element={<New />}></Route>
-            <Route path="/edit/:id" element={<Edit />}></Route>
-            <Route path="/diary/:id" element={<Diary />}></Route>
+            <>
+              <Route path="/" element={user ? <Home /> : <Login />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit/:id" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+              <Route path="/login" element={<Login />} />
+            </>
           </Routes>
         </div>
       </BrowserRouter>
